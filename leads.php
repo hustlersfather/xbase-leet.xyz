ab9af9afeded6a1be0335c5dca86c2818f4c44cb
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-25 10:09:28 -0800

    Update leads.php

--- leads.php
+++ leads.php
@@ -344,7 +344,79 @@ if ($r1 == "1") {
 
 
 </div>
-	  <script src="js/bootstrap.min.js"></script>
+	  <script src="seller/js/bootstrap.min.js">
+	  </script>
+	  <script type="text/javascript">
+$('#filterbutton').click(function () {
+$("#table tbody tr").each(function() {var ck1 = 
+		$.trim( $(this).find("#leads_country").text().toLowerCase() );var ck2 = 
+		$.trim( $(this).find("#leads_about").text().toLowerCase() );var ck3 = 
+		$.trim( $(this).find("#leads_seller").text().toLowerCase() ); var val1 = 
+		$.trim( $('select[name="leads_country"]').val().toLowerCase() );var val2 = 
+		$.trim( $('input[name="leads_about"]').val().toLowerCase() );var val3 = 
+		$.trim( $('select[name="leads_seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || (ck3 != val3 && val3 != '' )){ 
+		$(this).hide();  }else{ $(this).show(); } });
+
+$('#filterbutton').prop('disabled', true);
+});
+$('.filterselect').change(function () {
+	$('#filterbutton').prop('disabled', false);
+});
+$('.filterinput').keyup(function () {
+	$('#filterbutton').prop('disabled', false);
+});
+function buythistool(id){
+  bootbox.confirm("Are you sure?", function(result) {
+        if(result ==true){
+      $.ajax({
+     method:"GET",
+     url:"buytool.php?id="+id+"&t=leads",
+     dataType:"text",
+     success:function(data){
+         if(data.match(/<button/)){
+		 $("#leads"+id).html(data).show();
+         }else{
+            bootbox.alert('<center><img src="files/img/balance.png"><h2><b>No enough balance !</b>
+	    </h2>
+	    <h4>Please refill your balance <a class="btn btn-primary btn-xs"  href="addBalance.html" onclick="window.open(this.href);return false;" >Add Balance <span class="glyphicon glyphicon-plus"></span></a></h4></center>')
+         }
+     },
+   });
+       ;}
+  });
+}
+function openitem(order){
+  $("#myModalLabel").text('Order #'+order);
+  $('#myModal').modal('show');
+  $.ajax({
+    type:       'GET',
+    url:        'showOrder'+order+'.html',
+    success:    function(data)
+    {
+        $("#modelbody").html(data).show();
+    }});
+
+}
+
+</script>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel"></h4>
+      </div>
+      <div class="modal-body" id="modelbody">
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+	  
 </body>
 </html>
 
