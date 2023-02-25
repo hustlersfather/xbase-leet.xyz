483ecef6c4c7ab77d5ef7c123892d7b8bc6fbbba
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-25 12:16:09 -0800

    Update leads.php

--- leads.php
+++ leads.php
@@ -189,146 +189,91 @@ if ($r1 == "1") {
 	 
 </style>
 <div id=lead_table>
+<?php
+date_default_timezone_set('UTC');
+include "../includes/config.php";
 
-  <li class="active">
-  </li
-  </ul>
-  <div id="myTabContent" class="tab-content" >
-  <div class="tab-pane active in" id="filter">
-  <table class="table">
-		  <thead>
-			  <tr>
-				  <th>Country</th>
-				  <th>Description</th>
-				  <th>Seller</th>
-				  <th>search</th>
-			  </tr>
-		  </thead>
-		  <tbody>
-			  <tr>
-				  <td>
-					  <select class='filterselect form-control input-sm' name="leads_country">
-						  <option value="">ALL Countries</option>
-<?php $query = mysqli_query($dbcon, "SELECT DISTINCT(`country`) FROM `leads` WHERE `sold` = '0' ORDER BY country ASC");while($row = mysqli_fetch_assoc($query)){
-	echo '                           <option value="'.$row['country'].'">'.$row['country'].'</option>';
+if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
+    header("location: ../");
+    exit();
+}
+$usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
+?>
+
+<ul class="nav nav-tabs">
+  <li class="active"><a href="#filter" data-toggle="tab">Filter</a></li>
+</ul>
+<div id="myTabContent" class="tab-content" >
+  <div class="tab-pane active in" id="filter"><table class="table"><thead><tr><th>Country</th>
+<th>Description</th>
+<th>Seller</th>
+<th></th></tr></thead><tbody><tr><td><select class='filterselect form-control input-sm' name="leads_country"><option value="">ALL</option>
+<?php
+$query = mysqli_query($dbcon, "SELECT DISTINCT(`country`) FROM `leads` WHERE `sold` = '0' ORDER BY country ASC");
+	while($row = mysqli_fetch_assoc($query)){
+	echo '<option value="'.$row['country'].'">'.$row['country'].'</option>';
 	}
 ?>
-</select>
-	  </td>search<td>
-				  <input class='filterinput form-control input-sm' name="leads_about" size='3'>
-	  </td>...<td>
-				  <select class='filterselect form-control input-sm' name="leads_seller">
-					  <option value="">ALL Sellers</option>
-<?php $query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `leads` WHERE `sold` = '0' ORDER BY resseller ASC"); while($row = mysqli_fetch_assoc($query)){		 $qer = mysqli_query($dbcon, "SELECT DISTINCT(`id`) FROM resseller WHERE username='".$row['resseller']."' ORDER BY id ASC")or die(mysql_error());		   while($rpw = mysqli_fetch_assoc($qer))
-			                           $SellerNick = "seller".$rpw["id"]."";
-	echo '                         <option value="'.$SellerNick.'">'.$SellerNick.'</option>';
-}
+</select></td><td><input class='filterinput form-control input-sm' name="leads_about" size='3'></td><td><select class='filterselect form-control input-sm' name="leads_seller"><option value="">ALL</option>
+<?php
+$query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `leads` WHERE `sold` = '0' ORDER BY resseller ASC");
+	while($row = mysqli_fetch_assoc($query)){
+		 $qer = mysqli_query($dbcon, "SELECT DISTINCT(`id`) FROM resseller WHERE username='".$row['resseller']."' ORDER BY id ASC")or die(mysql_error());
+		   while($rpw = mysqli_fetch_assoc($qer))
+			 $SellerNick = "seller".$rpw["id"]."";
+	echo '<option value="'.$SellerNick.'">'.$SellerNick.'</option>';
+	}
 ?>
 </select></td><td><button id='filterbutton'class="btn btn-primary btn-sm" disabled>Filter <span class="glyphicon glyphicon-filter"></span></button></td></tr></tbody></table></div>
 </div>
 
 
-<table width="100%"  class="table table-striped table-bordered table-condensed sticky-header" id="lead_table">
+<table width="100%"  class="table table-striped table-bordered table-condensed sticky-header" id="table">
 <thead>
     <tr>
-	    <th data-priority="3">Country</th>
-	    <th data-priority="6">Description</th>
-	    
-	    <th data-priority="7">Email N</th>
-	    <th data-priority="8">Seller</th>
+      <th scope="col" >Country</th>
+      <th scope="col">Description</th>
+      <th scope="col">Email N</th>
 
-	    <th data-priority="9">Price</th>
-	    <th data-priority="10">Added on </th>
-	    <th class="all">Buy</th>
+      <th scope="col">Seller</th>
+      <th scope="col">Price</th>
+      <th scope="col">Added on </th>
+
+      <th scope="col">Buy</th>
     </tr>
 </thead>
   <tbody>
-</div>
-</div>
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
-<div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-notify modal-success" role="document">
-<div class="modal-content">
-<div class="modal-header">
-<p class="heading" id="myModalHeader"></p>
-<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-<span aria-hidden="true" class="white-text">&times;</span>
-</button>
-</div>
-<div class="modal-body" id="modelbody">
-</div>
-<div class="modal-footer justify-content-center">
-<a type="button" class="btn btn-outline-success waves-effect" data-dismiss="modal">Close</a>
-</div>
-</div>
-</div>
-</div>
 
-<div class="modal fade" id="modalConfirmBuy" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-<div class="modal-dialog modal-dialog-centered modal-sm modal-notify modal-info" role="document">
+ <?php
+include("cr.php");
+$q = mysqli_query($dbcon, "SELECT * FROM leads WHERE sold='0' ORDER BY RAND()")or die(mysqli_error());
+ while($row = mysqli_fetch_assoc($q)){
+	 
+	 	 $countryfullname = $row['country'];
+	  $code = array_search("$countryfullname", $countrycodes);
+	 $countrycode = strtolower($code);
+	    $qer = mysqli_query($dbcon, "SELECT * FROM resseller WHERE username='".$row['resseller']."'")or die(mysql_error());
+		   while($rpw = mysqli_fetch_assoc($qer))
+			 $SellerNick = "seller".$rpw["id"]."";
+     echo "
+ <tr>     
+    <td id='leads_country'><i class='flag-icon flag-icon-$countrycode'></i>&nbsp;".htmlspecialchars($row['country'])." </td>
+    <td id='leads_about'> ".htmlspecialchars($row['infos'])." </td> 
+	<td> ".htmlspecialchars($row['number'])." </td>
+    <td id='leads_seller'> ".htmlspecialchars($SellerNick)."</td>
+    <td> ".htmlspecialchars($row['price'])."</td>
+	    <td> ".$row['date']."</td>";
+    echo '
+    <td>
+	<span id="leads'.$row['id'].'" title="buy" type="leads"><a onclick="javascript:buythistool('.$row['id'].')" class="btn btn-primary btn-xs"><font color=white>Buy</font></a></span><center>
+    </td>
+            </tr>
+     ';
+ }
 
-<div class="modal-content text-center">
-
-<div class="modal-header d-flex justify-content-center">
-<p class="heading">Are you sure?</p>
-</div>
-
-<div class="modal-body">
-<i class='fas fa-shopping-cart fa-4x animated rotateIn'></i>
-</div>
-
-<div class="modal-footer flex-center">
-<a onClick='confirmbye()' class="btn btn-outline-info waves-effect" data-dismiss="modal">Yes</a>
-<a type="button" class="btn btn-info" data-dismiss="modal">No</a>
-</div>
-</div>
-
-</div>
-</div>
-
-
-<div class="modal fade top" id="modalCoupon" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" data-backdrop="true">
-<div class="modal-dialog modal-frame modal-top modal-notify modal-danger" role="document">
-
-<div class="modal-content">
-
-<div class="modal-body">
-<div class="row d-flex justify-content-center align-items-center">
-<img src="layout/images/balance.png">
-<span class="pt-3 mx-4" style="font-size: 14 px"><b>No enough balance !</b> Please refill your balance</span>
-<a type="button" href="addBalance" onclick="window.open(this.href);return false;" class="btn btn-danger">Add Balance
-<i class="fas fa-book ml-1 white-text"></i>
-</a>
-<a type="button" class="btn btn-outline-danger waves-effect" data-dismiss="modal">No, thanks</a>
-</div>
-</div>
-</div>
-
-</div>
-</div>
-
-<script>
-
-	  <script src="seller/js/bootstrap.min.js">
-	  </script>
-	  <script type="text/javascript">
-$('#filterbutton').click(function () {
-$("#table tbody tr").each(function() {var ck1 = 
-		$.trim( $(this).find("#leads_country").text().toLowerCase() );var ck2 = 
-		$.trim( $(this).find("#leads_about").text().toLowerCase() );var ck3 = 
-		$.trim( $(this).find("#leads_seller").text().toLowerCase() ); var val1 = 
-		$.trim( $('select[name="leads_country"]').val().toLowerCase() );var val2 = 
-		$.trim( $('input[name="leads_about"]').val().toLowerCase() );var val3 = 
-		$.trim( $('select[name="leads_seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || (ck3 != val3 && val3 != '' )){ 
-		$(this).hide();  }else{ $(this).show(); } });
-
-$('#filterbutton').prop('disabled', true);
-});
-$('.filterselect').change(function () {
-	$('#filterbutton').prop('disabled', false);
-});
-$('.filterinput').keyup(function () {
-	$('#filterbutton').prop('disabled', false);
-});
+ ?>
+<script type="text/javascript">
+$('#filterbutton').click(function () {$("#table tbody tr").each(function() {var ck1 = $.trim( $(this).find("#leads_country").text().toLowerCase() );var ck2 = $.trim( $(this).find("#leads_about").text().toLowerCase() );var ck3 = $.trim( $(this).find("#leads_seller").text().toLowerCase() ); var val1 = $.trim( $('select[name="leads_country"]').val().toLowerCase() );var val2 = $.trim( $('input[name="leads_about"]').val().toLowerCase() );var val3 = $.trim( $('select[name="leads_seller"]').val().toLowerCase() ); if((ck1 != val1 && val1 != '' ) || ck2.indexOf(val2)==-1 || (ck3 != val3 && val3 != '' )){ $(this).hide();  }else{ $(this).show(); } });$('#filterbutton').prop('disabled', true);});$('.filterselect').change(function () {$('#filterbutton').prop('disabled', false);});$('.filterinput').keyup(function () {$('#filterbutton').prop('disabled', false);});
 function buythistool(id){
   bootbox.confirm("Are you sure?", function(result) {
         if(result ==true){
@@ -340,9 +285,7 @@ function buythistool(id){
          if(data.match(/<button/)){
 		 $("#leads"+id).html(data).show();
          }else{
-            bootbox.alert('<center><img src="files/img/balance.png"><h2><b>No enough balance !</b>
-	    </h2>
-	    <h4>Please refill your balance <a class="btn btn-primary btn-xs"  href="addBalance.html" onclick="window.open(this.href);return false;" >Add Balance <span class="glyphicon glyphicon-plus"></span></a></h4></center>')
+            bootbox.alert('<center><img src="files/img/balance.png"><h2><b>No enough balance !</b></h2><h4>Please refill your balance <a class="btn btn-primary btn-xs"  href="addBalance.html" onclick="window.open(this.href);return false;" >Add Balance <span class="glyphicon glyphicon-plus"></span></a></h4></center>')
          }
      },
    });
@@ -363,5 +306,22 @@ function openitem(order){
 }
 
 </script>
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel"></h4>
+      </div>
+      <div class="modal-body" id="modelbody">
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
 </body>
 </html>
