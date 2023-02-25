2c2f3271abdcb438e89230d4194c201803a5de1f
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-25 23:29:44 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,26 +2,4 @@
 $(document).ready(function() {
     var table = $('#example').DataTable( {
         responsive: true
-    } );
- 
-    new $.fn.dataTable.FixedHeader( table );
-} );
-
-// Generate a random captcha code
-function generateCaptcha() {
-  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var captchaCode = '';
-  for (var i = 0; i < 6; i++) {
-    captchaCode += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return captchaCode;
-}
-
-// Display the captcha code in an image
-function displayCaptcha() {
-  var captchaCode = generateCaptcha();
-  var canvas = document.getElementById('captchaCanvas');
-  var context = canvas.getContext('2d');
-  context.font = '20px Arial';
-  context.strokeText(captchaCode, 10, 25);
-}
+    } 
