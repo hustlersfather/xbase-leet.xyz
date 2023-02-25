2fd471914f4a57af2a259b09d799db4b735da555
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-25 09:58:41 -0800

    Update divPage6.php

--- divPage6.php
+++ divPage6.php
@@ -13,23 +13,25 @@ $usrid = mysqli_real_escape_string($dbcon, $_SESSION['sname']);
 
 
 
- <?php
-include("cr.php");
-$q = mysqli_query($dbcon, "SELECT * FROM leads WHERE sold='0' ORDER BY RAND()")or die(mysqli_error());
- while($row = mysqli_fetch_assoc($q)){
-	 
-	 	 $countryfullname = $row['country'];
-	  $code = array_search("$countryfullname", $countrycodes);
-	 $countrycode = strtolower($code);
-	    $qer = mysqli_query($dbcon, "SELECT * FROM resseller WHERE username='".$row['resseller']."'")or die(mysql_error());
-		   while($rpw = mysqli_fetch_assoc($qer))
-			 $SellerNick = "seller".$rpw["id"]."";
-     echo "
- <tr>     
+ <?php include("cr.php"); 
+$q = mysqli_query($dbcon, "SELECT * FROM leads WHERE sold='0' ORDER BY RAND()")or die(mysqli_error()); while(
+	$row = mysqli_fetch_assoc($q)){ 
+	$countryfullname = $row['country']; 
+	$code = array_search("$countryfullname", 
+			     $countrycodes
+			    ); 
+	$countrycode = strtolower(
+		$code
+	); 
+	$qer = mysqli_query($dbcon, "SELECT * FROM resseller WHERE username='".$row['resseller']."'")or die(mysql_error()); while(
+		$rpw = mysqli_fetch_assoc($qer)) 
+		$SellerNick = "seller".$rpw["id"]."";
+	echo "<tr>     
     <td id='leads_country'><i class='flag-icon flag-icon-$countrycode'></i>&nbsp;".htmlspecialchars($row['country'])." </td>
     <td id='leads_about'> ".htmlspecialchars($row['infos'])." </td> 
 	<td> ".htmlspecialchars($row['number'])." </td>
-    <td id='leads_seller'> ".htmlspecialchars($SellerNick)."</td>
+    <td>
+    <span id='leads_seller'.$row['id'].'" title="seller" type="button"><a href="#" class="btn btn-primary btn-xs">".htmlspecialchars($SellerNick)."</td>
     <td> ".htmlspecialchars($row['price'])."</td>
 	    <td> ".$row['date']."</td>";
     echo '
