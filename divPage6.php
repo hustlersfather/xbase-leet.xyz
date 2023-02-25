e2907d71361019e370c09644c0aa0b211b2d6b39
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-25 12:09:56 -0800

    Update divPage6.php

--- divPage6.php
+++ divPage6.php
@@ -2,7 +2,7 @@
 ob_start();
 session_start();
 date_default_timezone_set('UTC');
-include "../includes/config.php";
+include "includes/config.php";
 
 if (!isset($_SESSION['sname']) and !isset($_SESSION['spass'])) {
     header("location: ../");
