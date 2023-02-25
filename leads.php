3916c9b66677a972a683b9a0a044bbb9da220a31
hustlersfathers <75556276+hustlersfather@users.noreply.github.com>
2023-02-25 09:04:29 -0800

    Update leads.php

--- leads.php
+++ leads.php
@@ -280,6 +280,54 @@ if ($r1 == "1") {
   </div>
   <!-- /.container-fluid -->
 </nav>
+<ul class="nav nav-tabs">
+  <li class="active"><a href="#filter" data-toggle="tab">Filter</a></li>
+</ul>
+<div id="myTabContent" class="tab-content" >
+  <div class="tab-pane active in" id="filter"><table class="table"><thead><tr><th>Country</th>
+<th>Description</th>
+<th>Seller</th>
+<th></th></tr></thead><tbody><tr><td><select class='filterselect form-control input-sm' name="leads_country"><option value="">ALL</option>
+<?php
+$query = mysqli_query($dbcon, "SELECT DISTINCT(`country`) FROM `leads` WHERE `sold` = '0' ORDER BY country ASC");
+	while($row = mysqli_fetch_assoc($query)){
+	echo '<option value="'.$row['country'].'">'.$row['country'].'</option>';
+	}
+?>
+</select>
+	  </td><td>
+	  <input class='filterinput form-control input-sm' name="leads_about" size='3'>
+	  </td><td>
+	  <select class='filterselect form-control input-sm' name="leads_seller">
+		  <option value="">ALL</option>
+<?php
+$query = mysqli_query($dbcon, "SELECT DISTINCT(`resseller`) FROM `leads` WHERE `sold` = '0' ORDER BY resseller ASC");
+	while($row = mysqli_fetch_assoc($query)){
+		 $qer = mysqli_query($dbcon, "SELECT DISTINCT(`id`) FROM resseller WHERE username='".$row['resseller']."' ORDER BY id ASC")or die(mysql_error());
+		   while($rpw = mysqli_fetch_assoc($qer))
+			 $SellerNick = "seller".$rpw["id"]."";
+	echo '<option value="'.$SellerNick.'">'.$SellerNick.'</option>';
+	}
+?>
+</select></td><td><button id='filterbutton'class="btn btn-primary btn-sm" disabled>Filter <span class="glyphicon glyphicon-filter"></span></button></td></tr></tbody></table></div>
+</div>
+
+
+<table width="100%"  class="table table-striped table-bordered table-condensed sticky-header" id="table">
+<thead>
+    <tr>
+      <th scope="col" >Country</th>
+      <th scope="col">Description</th>
+      <th scope="col">Email N</th>
+
+      <th scope="col">Seller</th>
+      <th scope="col">Price</th>
+      <th scope="col">Added on </th>
+
+      <th scope="col">Buy</th>
+    </tr>
+</thead>
+  <tbody>
 <div id="mainDiv">
 
 
